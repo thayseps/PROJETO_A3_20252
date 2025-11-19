d19c8ef73bc031090d5454aa22f13631f039cee2
thayseps <thaypereirasantos@gmail.com>
2025-11-18 21:15:46 -0300

    implementacao do search

--- apps/products/views.py
+++ apps/products/views.py
@@ -3,9 +3,13 @@ from django.shortcuts import render
 from .models import Product
 from rest_framework import viewsets
 from .serializer import ProductSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer  
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'description', 'category'] 
+    
